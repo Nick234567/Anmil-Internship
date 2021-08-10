ad89cae36804b5e583c4819156aea2927e6f5ab7
Nick234567 <Nickupadhyaya123@gmail.com>
2021-08-10 10:59:16 +0545

    new net salary calculator

--- net-salary-calculator.js
+++ net-salary-calculator.js
@@ -1,75 +1,41 @@
-const salaryArray = [
+const persons = [
   { name: "John Doe", salary: 9500 },
   { name: "Jane Doe", salary: 16500 },
   { name: "Jim Doe", salary: 24500 },
-  { name: "Josh Doe", salary: 37000 },
+  { name: "Josh Doe", salary: 45000 },
 ];
 
-const printOutput = (name, salary) => {
-  console.log(`${name} salary is ${salary}`);
+const taxSlabs = [
+  { gt: 0, lte: 10000, tax: 2 },
+  { gt: 10000, lte: 20000, tax: 4 },
+  { gt: 20000, lte: 30000, tax: 7 },
+  { gt: 30000, lte: 40000, tax: 10 },
+  { gt: 40000, lte: 999999, tax: 12 }
+];
+
+const netTax = (salary) => {
+  let remainderGrossSalary = salary;
+  let totalTaxAmount = 0;
+  taxSlabs.forEach((slab) => {
+    const range = slab.lte - slab.gt;
+    let taxableAmount = 0;
+    let taxAmount = 0;
+
+    if (remainderGrossSalary <= range) {
+      taxableAmount = remainderGrossSalary;
+      remainderGrossSalary = 0;
+    } else {
+      taxableAmount = range;
+      remainderGrossSalary = remainderGrossSalary - range;
+    }
+
+    taxAmount = taxableAmount * (slab.tax / 100);
+    totalTaxAmount += taxAmount;
+  });
+
+  return Math.ceil(totalTaxAmount);
 };
 
-
-salaryArray.forEach(({ name, salary }) => {
-  const twoPercentTax = (2 * 10000) / 100;
-  const fourPercentTax = (4 * 10000) / 100;
-  const sevenPercentTax = (7 * 10000) / 100;
-
-  if (salary > 0 && salary <= 10000) {
-
-    //Net salary of John Doe
-
-    const netSalary = salary - (2 * salary) / 100;     
-    printOutput(name, netSalary);                   
-  } 
-  
-  else if (salary > 10000 && salary <= 20000)
-   {
-     // Tax on remainig salary after Ten thousand 
-    const remainingTax1 = (4 * (salary - 10000)) / 100;
-  
-    //Total tax if salary is > 10000 or <= 20000
-
-    const totalTax =
-      twoPercentTax + remainingTax1;
-
-        //Net salary of Jane Doe
-
-    const netSalary = salary - totalTax;                           
-    printOutput(name, netSalary);
-  } 
-  else if (salary > 20000 && salary <= 30000) 
-  {
-
-    // Tax on Remaining Salary After Twenty Thousand
-    const remainingTax2 =
-      (7 * (salary - 20000)) / 100;
-   
-   //Total tax if salary is > 20000 or <= 30000
-      const totalTax =
-      twoPercentTax +
-      fourPercentTax +
-     remainingTax2;
-
-     //Net salary of Jim Doe
-     
-    const netSalary = salary - totalTax;                     
-    printOutput(name, netSalary);
-  }
-   else if (salary > 30000) 
-   {
-
-    // Tax on remaining salary after thrity thousand 
-    const remainingTax3 =
-      (10 * (salary - 30000)) / 100;
-
-      //Total tax if salary is >30000
-      const totalTax =
-      twoPercentTax +
-      fourPercentTax +
-      sevenPercentTax +
-      remainingTax3;
-    const netSalary = salary - totalTax;                        //Net salary of John Doe
-    printOutput(name, netSalary);
-  }
+persons.map((m) => {
+  console.log(`${m.name} salary is  ${m.salary - netTax(m.salary)}`);
 });
