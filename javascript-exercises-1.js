61a4a17bae8ddbd0eb9af403373c010953a0b23d
Nick234567 <Nickupadhyaya123@gmail.com>
2021-08-11 12:09:40 +0545

    function changed into prototype function

--- javascript-exercises-1.js
+++ javascript-exercises-1.js
@@ -1,43 +1,16 @@
 //Ex: 1, Start
-let word1 = "level";
-let word2 = "shovel";
-function check_palindrome( str )
-  {
-    let j = str.length -1;
-    for( let i = 0 ; i < j/2 ;i++)
-    {
-      let x = str[i] ;//forward character
-      
-      let y = str[j-i];//backward character
-      if( x != y)
-      {
-        // return false if given string is not matched
-        return false;
-      }
-    }
-    /// return true if given string is matched
-    return true;
-     
-  }
- 
-  function is_palindrome( str )
-  {
-   
-    let result = check_palindrome(str);
-    
-    if( result == true )
-    {
-      console.log("The string word1 is pallindrome");
-    }
-    else
-    {
-      console.log("The string word2 is not a pallindrome");
-    }
-  }
-  
-  is_palindrome(word1);
-  is_palindrome(word2);
+function string(str) {
+  this.str = str;
+}
+string.prototype.isPallindrome = function () {
+  return (
+    this.str.toString() === this.str.toString().split("").reverse().join("")
+  );
+};
 
+const myString = new string("level");
+
+console.log(myString.isPallindrome());
   //Ex: 1, End
 
 
@@ -73,6 +46,22 @@ console.log(result)
 
 //Ex:4, End
 
+
+//Ex:5, Start
+
+const points = { p1: [2, 3], p2: [6, 7]};
+
+let x1 = 2;
+let y1 = 3;
+let x2 = 6;
+let y2= 7;
+
+let x = y2 -y1;
+let y = x2 - x1;
+
+let displacement = (math.sqrt(((x-2)-(x-1)*(x-2)-(x-1)) +((y-2)-(y-1)*(y-2)-(y-1)) ));
+
+console.log(displacement)
 
 
   
